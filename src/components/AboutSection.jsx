d0c22c9552e7513c46aaca81dc33b9bcf8baff04
bhanu prudhvi <61325669+bhanuprudhvi@users.noreply.github.com>
2025-07-30 09:44:26 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -27,7 +27,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="/BhanuPrudhviDanda_SDE_Resume.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download Resume
